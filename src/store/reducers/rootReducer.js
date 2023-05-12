0e5ca47dd2c9eaa91e5ccd69118e96243423c4d2
buithihatien2711 <buithihatien2711@gmail.com>
2023-05-12 16:49:15 +0700

    map state(redux) to props(react)

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    user : [
+        {id: 1, name: 'Tien'},
+        {id: 2, name: 'React'}
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+
+    return state
+}
+
+export default rootReducer
--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router'
 import Color from '../HOC/Color'
 import homeImage from '../../assets/images/image-home.jpg'
+import {connect} from 'react-redux'
 
 class Home extends Component {
     // componentDidMount() {
@@ -13,6 +14,7 @@ class Home extends Component {
     render() {
         return (
             <>
+                {console.log('>>> check props: ', this.props)}
                 <div>Hello world with React</div>
                 <div>
                     <img src={homeImage} alt="" style={{width: '400px', height: 'auto', marginTop:'20px'}}/>
@@ -22,4 +24,12 @@ class Home extends Component {
     }
 }
 
-export default Color(Home)
+// export default Color(Home)
+
+const mapStateToProps = (state) => {
+    return {
+        data: state.user
+    }
+}
+
+export default connect(mapStateToProps)(Color(Home))
